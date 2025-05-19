966bc424f673b279ddd49e7a97468c1c065adcb9
yepot <yes03326@gmail.com>
2025-05-19 14:44:22 +0900

    refactor: extract DB connection logic to DBConnection and connect from Main

--- dbdbd/src/dbdbd/Main.java
+++ dbdbd/src/dbdbd/Main.java
@@ -1,9 +1,22 @@
 package dbdbd;
 
+import dbdbd.DBConnection;
+import java.sql.Connection;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// 연결 테스트
+		try (Connection conn = DBConnection.getConnection()) {
+            System.out.println(">>> DB 연결 성공!");
+
+
+        } catch (Exception e) {
+            System.err.println(">>> DB 연결 실패!");
+            e.printStackTrace();
+        }
 
 	}
 
