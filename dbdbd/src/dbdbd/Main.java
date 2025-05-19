9e981e32a32933ce45830651aaecf9cd59780287
wys0530 <166965895+wys0530@users.noreply.github.com>
2025-05-19 15:53:23 +0900

    Merge branch 'main' into yunsoo
    'Main.java' update

--- dbdbd/src/dbdbd/Main.java
+++ dbdbd/src/dbdbd/Main.java
@@ -1,23 +1,37 @@
 package dbdbd;
 
 import dbdbd.DBConnection;
+import dbdbd.service.*;
+
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Connection conn=null;
 		// 연결 테스트
-		try (Connection conn = DBConnection.getConnection()) {
+		try{
+			conn = DBConnection.getConnection();
             System.out.println(">>> DB 연결 성공!");
-
+       
 
         } catch (Exception e) {
             System.err.println(">>> DB 연결 실패!");
             e.printStackTrace();
-        }
+        }finally {
+            // 자원 해제
+            if (conn != null) {
+                try {
+                    conn.close();
+                    System.out.println("... Close Connection"+ conn.toString()+" ...");
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
 
 	}
 
+}
 }
