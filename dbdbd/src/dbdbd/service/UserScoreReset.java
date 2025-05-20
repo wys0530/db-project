e56e077ab8025b8127c0c2a863a869ca806a39dc
greenxlouv <Jesuisjaelin16@gmail.com>
2025-05-21 02:52:13 +0900

    제목 부분 수정

--- dbdbd/src/dbdbd/service/UserScoreReset.java
+++ dbdbd/src/dbdbd/service/UserScoreReset.java
@@ -1,5 +1,5 @@
 package dbdbd.service;
-
+import java.util.*;
 import java.sql.*;
 import java.util.Scanner;
 
@@ -44,11 +44,10 @@ public class UserScoreReset {
 
 
 			//2. 조건에 맞는 게임 목록 조회
+			Set<String> validTitles = new HashSet<>(); // 유효한 게임 제목 저장용
 			sql_select="""
 						select ph.user_id,title,total_playtime,last_play,user_score
 						from play_history ph
-						left outer join review r 
-						on ph.user_id = r.user_id AND ph.game_id = r.game_id
 						inner join game g on ph.game_id = g.game_id
 						where ph.user_id=? and (ph.total_playtime<=1500 or ph.last_play<=date_sub(now(),interval 6 month))
 					""";
@@ -66,11 +65,17 @@ public class UserScoreReset {
 				
 				System.out.printf("제목: %s | 플레이타임: %d | 마지막 플레이: %s | 점수: %d%n",
 						title, playtime, lastPlay, score);
+				validTitles.add(title); // 제목 저장
 			}
 
-			//3. user_score를 초기화할 게임 선 >> title 기준
+			//3. user_score를 초기화할 게임 선택  >> title 기준
 			System.out.print("\nuser_score을 0으로 초기화할 게임 제목을 입력 :");
 			String selectedT=sc.nextLine();
+			
+			if (!validTitles.contains(selectedT)) {
+			    System.out.println("제목을 잘못 입력하였습니다.");
+			    return; // 종료
+			}
 
 			//4. 해당 title을 기준으로 업데이트
 			sql_update="""
